15386654fb4f9743c674ed055ada374d2f2723fc
Loic Devulder <ldevulder@suse.com>
2026-04-21 10:34:04 +0200

    Fix linter issues
    Signed-off-by: Loic Devulder <ldevulder@suse.com>

--- tests/qase_test.go
+++ tests/qase_test.go
@@ -1,10 +1,13 @@
 /*
-Copyright © 2023 - 2024 SUSE LLC
+Copyright © 2023 - 2026 SUSE LLC
+SPDX-License-Identifier: Apache-2.0
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,7 +32,7 @@ func outputCurrentPwd() ([]byte, error) {
 }
 
 var _ = Describe("Qase Ginkgo Integration - Unit tests", func() {
-	var qaseRunID int32
+	var qaseRunID int64
 
 	It("Create and export a Qase Run", func() {
 		qaseRunID = CreateRun()
